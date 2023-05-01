a67bd88a4b5e97b5f12431cb3017751883997fe9
Miroslava Osorio <miroslavaosorio1@gmail.com>
2023-05-01 06:43:05 +0000

    delete completed

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,12 +2,25 @@ class PhotosController < ApplicationController
 
   def index
     matching_photos = Photo.all
-
-
     @list_of_photos = matching_photos.order({ :created_at => :desc })
-
     render({ :template => "photo_templates/index.html.erb" })
   end
 
+  def show
+    # Parameters: {"path_id"=>"777"}
+    url_id = params.fetch("path_id")
+    matching_photos = Photo.where({ :id => url_id })
+    @the_photo = matching_photos.at(0)
+    render({ :template => "photo_templates/show.html.erb" })
+  end
 
+  def bye
+    # Parameters: {"toast_id"=>"785"}
+    the_id = params.fetch("toast_id")
+    matching_photos = Photo.where({ :id => the_id })
+    the_photo = matching_photos.at(0)
+    the_photo.destroy
+    # render({ :template => "photo_templates/bye.html.erb" })
+    redirect_to("/photos")
+  end
 end
